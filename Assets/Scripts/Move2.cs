037f60cc8a3d4a4a187d415bee4086aefcf4dcf3
keponer <alroigar@upv.es>
2016-08-08 15:01:01 +0200

    f

---
+++ Assets/Scripts/Move2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move2 : MonoBehaviour {
+
+    public float forceValue, jumpValue;
+    private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Jump") && Mathf.Abs(rigidbody.velocity.y) < 0.01f){
+            rigidbody.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+           }
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * forceValue);
+    }
+}
